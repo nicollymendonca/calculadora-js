e75753e17f0fb3c4a55b4369173a2ff4eaf282ec
nicollymendonca <nicollytefane280@gmail.com>
2022-09-18 14:19:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+function adicao(){
+    let n1 = parseInt(document.getElementById("a").value);
+    let n2 = parseInt(document.getElementById("b").value);
+    console.log(n1+n2);
+    }
+    
+    function subtracao(){
+    let n1 = parseInt(document.getElementById("a").value);
+    let n2 = parseInt(document.getElementById("b").value);
+    console.log(n1-n2);
+    }
+    
+    function multiplicacao(){
+    let n1 = parseInt(document.getElementById("a").value);
+    let n2 = parseInt(document.getElementById("b").value);
+    console.log(n1*n2);
+    }
+    
+    function divisao(){
+    let n1 = parseInt(document.getElementById("a").value);
+    let n2 = parseInt(document.getElementById("b").value);
+    console.log(n1/n2);
+    }
+    
+    function modDiv(){
+    let n1 = parseInt(document.getElementById("a").value);
+    let n2 = parseInt(document.getElementById("b").value);
+    console.log(n1%n2);
+    }
+    
+    function incremento(){
+    let n1 = parseInt(document.getElementById("a").value);
+    let n2 = parseInt(document.getElementById("b").value);
+    console.log(n1++);
+    }
+    
+    function decremento(){
+    let n1 = parseInt(document.getElementById("a").value);
+    let n2 = parseInt(document.getElementById("b").value);
+    console.log(n1--);
+    }
